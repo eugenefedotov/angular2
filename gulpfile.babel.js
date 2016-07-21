73c46bf79423ac7dc470573b717cacf63d250faf
FENIX <fenix123fly@gmail.com>
2016-07-21 16:17:26 +0300

    add vinyl-source-stream for browserify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,32 +2,48 @@ import gulp from 'gulp';
 import gless from 'gulp-less';
 import gutil from 'gulp-util';
 import browserify from 'browserify';
+import source from 'vinyl-source-stream';
 import tsify from 'tsify';
 
-let devFolder = './app/';
-let destFolder = './build/';
+let devFolder = './app';
+let destFolder = './build';
 
 gulp.task('browserify', () => {
-
+  return browserify({
+    entries : 'app/bootstrap.ts',
+    debug   : true
+  })
+    .plugin(tsify, {
+      target                 : 'es5',
+      experimentalDecorators : true
+    })
+    .bundle()
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red.bold('[browserify error]'));
+      gutil.log(err.message);
+      this.emit('end');
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(destFolder));
 });
 
 gulp.task('less', () => {
   return gulp
-    .src(devFolder + 'less/style.less')
+    .src(devFolder + '/less/style.less')
     .pipe(gless())
     .on('error', (err) => {
       gutil.log(gutil.colors.red.bold('[Less error]'));
-      gutil.log(gutil.colors.bgRed('filename: ') +' '+ err.filename);
-      gutil.log(gutil.colors.bgRed('lineNumber: ') +' '+ err.lineNumber);
+      gutil.log(gutil.colors.bgRed('filename: ') + ' ' + err.filename);
+      gutil.log(gutil.colors.bgRed('lineNumber: ') + ' ' + err.lineNumber);
       gutil.log(gutil.colors.bgRed('extract: ') + err.extract.join(' '));
     })
     .pipe(gulp.dest())
 });
 
 gulp.task('watch', () => {
-  gulp.watch(devFolder + '**/*.ts', ['browserify']);
-  gulp.watch(devFolder + 'less/*.less', ['less']);
+  gulp.watch(devFolder + '/**/*.ts', ['browserify']);
+  gulp.watch(devFolder + '/less/*.less', ['less']);
 });
 
-gulp.task('dist', ['browserify', 'less']);
-gulp.task('default', ['dist', 'watch']);
+gulp.task('build', ['browserify', 'less']);
+gulp.task('default', ['build', 'watch']);
