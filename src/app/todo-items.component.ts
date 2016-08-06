e7f547963b3138e48ec4cde7c12cdba653e7efbd
FENIX <fenix123fly@gmail.com>
2016-08-06 12:27:56 +0300

    fixed styles add new component import new component

---
+++ src/app/todo-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-items',
+  template: require('./todo-items.component.html'),
+  styles: [
+    require('./todo-items.component.css')
+  ]
+})
+
+export class ToDoItemsComponent {}
--- src/app/todo-list.component.ts
+++ src/app/todo-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ToDoItemsComponent } from './todo-items.component'
 
 @Component({
   selector: 'todo-list',
   template: require('./todo-list.component.html'),
   styles: [
     require('./todo-list.component.css')
-  ]
+  ],
+  directives: [ToDoItemsComponent]
 })
 
 export class ToDoListComponent {}
